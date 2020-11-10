d6459fbcde0acb967381b88f874c474710afbd35
Alejo-Casta <alejo.casta.al@gmail.com>
2020-11-09 21:36:22 -0500

    Instalación de dependecias: Material, hammer, temas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FlexLayoutModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
